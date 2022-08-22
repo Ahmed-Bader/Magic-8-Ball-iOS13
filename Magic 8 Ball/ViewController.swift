0830118941af39cfd78f0248170ef885074c5571
Ahmed the Green <67065998+Ahmed-Bader@users.noreply.github.com>
2022-08-22 11:58:02 +0300

    finished using int.random

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,11 +12,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    let ballArray = [UIImage(named:"ball1"),
+                     UIImage(named:"ball2"),
+                     UIImage(named:"ball3"),
+                     UIImage(named:"ball4"),
+                     UIImage(named:"ball5")]
+    
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        imageView.image = UIImage(named: "ball3")
+        imageView.image = ballArray[Int.random(in: 0..<ballArray.count)]
+        
+        //UIImage(named: "ball3")
     }
     
-    let ballArray = [#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
 
 
 }
